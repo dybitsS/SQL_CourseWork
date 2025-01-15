78df65ced34099338786bd959a8739ef415724ed
Denis Lukashevich <dybitsss@gmail.com>
2025-01-15 16:56:58 +0100

    Re work some ETL process

--- ETL from oltp to olap.sql
+++ ETL from oltp to olap.sql
@@ -1,30 +1,48 @@
 INSERT INTO DimDates (Date, Year, Month, Day, Week, Quarter)
-SELECT DISTINCT o."OrderDate",
-                EXTRACT(YEAR FROM o."OrderDate")::int,
-                EXTRACT(MONTH FROM o."OrderDate")::int,
-                EXTRACT(DAY FROM o."OrderDate")::int,
-                EXTRACT(WEEK FROM o."OrderDate")::int,
-                CASE 
-                    WHEN EXTRACT(MONTH FROM o."OrderDate") IN (1, 2, 3) THEN 1
-                    WHEN EXTRACT(MONTH FROM o."OrderDate") IN (4, 5, 6) THEN 2
-                    WHEN EXTRACT(MONTH FROM o."OrderDate") IN (7, 8, 9) THEN 3
-                    WHEN EXTRACT(MONTH FROM o."OrderDate") IN (10, 11, 12) THEN 4
-                END AS Quarter
+SELECT DISTINCT 
+    o."OrderDate",
+    EXTRACT(YEAR FROM o."OrderDate")::int,
+    EXTRACT(MONTH FROM o."OrderDate")::int,
+    EXTRACT(DAY FROM o."OrderDate")::int,
+    EXTRACT(WEEK FROM o."OrderDate")::int,
+    CASE 
+        WHEN EXTRACT(MONTH FROM o."OrderDate") IN (1, 2, 3) THEN 1
+        WHEN EXTRACT(MONTH FROM o."OrderDate") IN (4, 5, 6) THEN 2
+        WHEN EXTRACT(MONTH FROM o."OrderDate") IN (7, 8, 9) THEN 3
+        WHEN EXTRACT(MONTH FROM o."OrderDate") IN (10, 11, 12) THEN 4
+    END AS Quarter
 FROM dblink(
     'dbname=oltp_database user=postgres password=123 host=localhost',
-    'SELECT DISTINCT "OrderDate" FROM public."Orders"')
-AS o("OrderDate" date)
-WHERE NOT EXISTS (SELECT 1 FROM DimDates dd WHERE dd.Date = o."OrderDate");
+    'SELECT DISTINCT "OrderDate" FROM public."Orders"'
+) AS o("OrderDate" date)
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM DimDates dd 
+    WHERE dd.Date = o."OrderDate"
+)
+UNION
+SELECT 
+    CURRENT_DATE,
+    EXTRACT(YEAR FROM CURRENT_DATE)::int,
+    EXTRACT(MONTH FROM CURRENT_DATE)::int,
+    EXTRACT(DAY FROM CURRENT_DATE)::int,
+    EXTRACT(WEEK FROM CURRENT_DATE)::int,
+    EXTRACT(QUARTER FROM CURRENT_DATE)::int
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM DimDates dd 
+    WHERE dd.Date = CURRENT_DATE
+);
 
-INSERT INTO DimCities (CitySK,CityName)
-SELECT DISTINCT "CityID","Name"
+INSERT INTO DimCities (CitySK, CityName)
+SELECT DISTINCT "CityID", "CityName"
 FROM dblink(
     'dbname=oltp_database user=postgres password=123 host=localhost',
-    'SELECT "CityID", "CityName" FROM public."Cities"')
-	as cities("CityID" int, "Name" varchar)
-WHERE NOT EXISTS (SELECT 1 FROM DimCities dc WHERE dc.CityName = "Name");
+    'SELECT "CityID", "CityName" FROM public."Cities"'
+) AS cities("CityID" int, "CityName" varchar)
+WHERE NOT EXISTS (SELECT 1 FROM DimCities dc WHERE dc.CityName = cities."CityName");
 
-INSERT INTO DimAddresses (AddressSK,Street, CitySK, PostalCode, Country)
+INSERT INTO DimAddresses (AddressSK, Street, CitySK, PostalCode, Country)
 SELECT DISTINCT a."AddressID",
                 a."Street", 
                 a."CityID", 
@@ -32,124 +50,110 @@ SELECT DISTINCT a."AddressID",
                 a."Country"
 FROM dblink(
     'dbname=oltp_database user=postgres password=123 host=localhost',
-    'SELECT "AddressID", "Street", "CityID", "PostalCode", "Country" FROM public."Addresses"')
-AS a("AddressID" int, "Street" varchar, "CityID" int, "PostalCode" varchar, "Country" varchar)
-WHERE NOT EXISTS (SELECT 1 FROM DimAddresses da WHERE da.Street = a."Street" AND da.PostalCode = a."PostalCode" and da.Country = a."Country");
+    'SELECT "AddressID", "Street", "CityID", "PostalCode", "Country" FROM public."Addresses"'
+) AS a("AddressID" int, "Street" varchar, "CityID" int, "PostalCode" varchar, "Country" varchar)
+WHERE NOT EXISTS (SELECT 1 FROM DimAddresses da WHERE da.Street = a."Street" AND da.PostalCode = a."PostalCode" AND da.Country = a."Country");
 
-INSERT INTO DimCategories (CategorySK,Name)
-SELECT DISTINCT "CategoryID","Name"
+INSERT INTO DimCategories (CategorySK, Name)
+SELECT DISTINCT "CategoryID", "Name"
 FROM dblink(
     'dbname=oltp_database user=postgres password=123 host=localhost',
-    'SELECT "CategoryID", "Name" FROM public."Categories"')
-AS categories("CategoryID" int, "Name" varchar)
-WHERE NOT EXISTS (SELECT 1 FROM DimCategories dc WHERE dc.Name = "Name");
+    'SELECT "CategoryID", "Name" FROM public."Categories"'
+) AS categories("CategoryID" int, "Name" varchar)
+WHERE NOT EXISTS (SELECT 1 FROM DimCategories dc WHERE dc.Name = categories."Name");
 
-INSERT INTO DimConditions (ConditionSK,Name)
-SELECT DISTINCT "ConditionId","Name"
+INSERT INTO DimConditions (ConditionSK, Name)
+SELECT DISTINCT "ConditionId", "Name"
 FROM dblink(
     'dbname=oltp_database user=postgres password=123 host=localhost',
-    'SELECT "ConditionId", "Name" FROM public."Condition"')
-AS conditions("ConditionId" int, "Name" varchar)
-WHERE NOT EXISTS (SELECT 1 FROM DimConditions dc WHERE dc.Name = "Name");
+    'SELECT "ConditionId", "Name" FROM public."Condition"'
+) AS conditions("ConditionId" int, "Name" varchar)
+WHERE NOT EXISTS (SELECT 1 FROM DimConditions dc WHERE dc.Name = conditions."Name");
 
-INSERT INTO DimStatus (StatusSK,Name)
-SELECT DISTINCT "StatusID","Name"
+INSERT INTO DimStatus (StatusSK, Name)
+SELECT DISTINCT "StatusID", "Name"
 FROM dblink(
     'dbname=oltp_database user=postgres password=123 host=localhost',
-    'SELECT "StatusID", "Name" FROM public."Status"')
-AS status("StatusID" int, "Name" varchar)
-WHERE NOT EXISTS (SELECT 1 FROM DimStatus ds WHERE ds.Name = "Name");
+    'SELECT "StatusID", "Name" FROM public."Status"'
+) AS status("StatusID" int, "Name" varchar)
+WHERE NOT EXISTS (SELECT 1 FROM DimStatus ds WHERE ds.Name = status."Name");
 
-INSERT INTO DimPickUp (PickUpSK,Name)
-SELECT DISTINCT "PickUpID","DeliveryType"
+INSERT INTO DimPickUp (PickUpSK, Name)
+SELECT DISTINCT "PickUpID", "DeliveryType"
 FROM dblink(
     'dbname=oltp_database user=postgres password=123 host=localhost',
-    'SELECT "PickUpID", "DeliveryType" FROM public."PickUpOptions"')
-AS pickupoptions("PickUpID" int, "DeliveryType" varchar)
+    'SELECT "PickUpID", "DeliveryType" FROM public."PickUpOptions"'
+) AS pickupoptions("PickUpID" int, "DeliveryType" varchar)
 WHERE NOT EXISTS (
     SELECT 1 
     FROM DimPickUp dpo 
     WHERE dpo.Name = pickupoptions."DeliveryType");
 
 
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM DimDates WHERE Date = CURRENT_DATE) THEN
-        INSERT INTO DimDates (Date, Year, Month, Day, Week, Quarter)
-        VALUES (
-            CURRENT_DATE,
-            EXTRACT(YEAR FROM CURRENT_DATE)::int,
-            EXTRACT(MONTH FROM CURRENT_DATE)::int,
-            EXTRACT(DAY FROM CURRENT_DATE)::int,
-            EXTRACT(WEEK FROM CURRENT_DATE)::int,
-            EXTRACT(QUARTER FROM CURRENT_DATE)::int
-        );
-    END IF;
-END $$;
+WITH updated_products AS (
+    SELECT 
+        p."ProductID",
+    	p."Name", 
+    	p."CategoryID", 
+    	p."ConditionID", 
+   		p."Era", 
+   		p."Price",
+        (SELECT DatesKey FROM DimDates WHERE Date = CURRENT_DATE) AS StartDateID
+	FROM dblink(
+    	'dbname=oltp_database user=postgres password=123 host=localhost',
+    	'SELECT "ProductID", "Name", "CategoryID", "ConditionID", "Era", "Price" FROM public."Products"'
+	) AS p("ProductID" int, "Name" varchar, "CategoryID" int, "ConditionID" int, "Era" varchar, "Price" decimal)
+    LEFT JOIN DimProducts dp ON dp.ProductSK = p."ProductID"
+    WHERE dp.ProductSK IS NULL OR (dp.IsCurrent AND (TRIM(p."Name") <> TRIM(dp.Name)
+        OR TRIM(p."Era") <> TRIM(dp.Era)
+        OR p."CategoryID" <> dp.CategorySK
+        OR p."ConditionID" <> dp.ConditionSK
+		OR p."Price" <> dp.Price))),
+update_product AS (
+    UPDATE DimProducts
+    SET 
+        IsCurrent = false,
+        EndDateID = (SELECT DatesKey FROM DimDates WHERE Date = CURRENT_DATE)
+    WHERE 
+        ProductSK IN (SELECT "ProductID" FROM updated_products) AND IsCurrent = true
+)
+INSERT INTO DimProducts (ProductSK, Name, CategorySK, ConditionSK, Era, Price, IsCurrent, StartDateID)
+    SELECT "ProductID", "Name", "CategoryID", "ConditionID", "Era", "Price", true, StartDateID
+    FROM updated_products;
 
-INSERT INTO DimProducts (ProductSK,Name, CategorySK, ConditionSK, Era, Price, IsCurrent, StartDateID)
-SELECT DISTINCT
-	p."ProductID",
-    p."Name", 
-    p."CategoryID", 
-    p."ConditionID", 
-    p."Era", 
-    p."Price", 
-    TRUE, 
-    (SELECT datesk FROM dimdates WHERE date = CURRENT_DATE)
-FROM dblink(
-    'dbname=oltp_database user=postgres password=123 host=localhost',
-    'SELECT "ProductID", "Name", "CategoryID", "ConditionID", "Era", "Price" FROM public."Products"'
-) AS p("ProductID" int, "Name" varchar, "CategoryID" int, "ConditionID" int, "Era" varchar, "Price" decimal)
-JOIN DimCategories c ON c.CategorySK = p."CategoryID"
-JOIN DimConditions co ON co.ConditionSK = p."ConditionID"
-ON CONFLICT (Name) DO UPDATE SET
-    CategorySK = EXCLUDED.CategorySK,
-    ConditionSK = EXCLUDED.ConditionSK,
-    Era = EXCLUDED.Era,
-    Price = EXCLUDED.Price,
-    IsCurrent = EXCLUDED.IsCurrent,
-    StartDateID = EXCLUDED.StartDateID
-WHERE 
-    DimProducts.CategorySK <> EXCLUDED.CategorySK OR
-    DimProducts.ConditionSK <> EXCLUDED.ConditionSK OR
-    DimProducts.Era <> EXCLUDED.Era OR
-    DimProducts.Price <> EXCLUDED.Price OR
-    DimProducts.IsCurrent <> EXCLUDED.IsCurrent OR
-    DimProducts.StartDateID <> EXCLUDED.StartDateID;
+WITH updated_customers AS (
+    SELECT 
+        c."ClientID",
+        c."Name",
+        c."Email",
+        c."PhoneNumber",
+        c."AddressID",
+        c."Preferences_Category",
+        (SELECT DatesKey FROM DimDates WHERE Date = CURRENT_DATE) AS StartDateID
+    FROM dblink(
+        'dbname=oltp_database user=postgres password=123 host=localhost',
+        'SELECT "ClientID", "Name", "Email", "PhoneNumber", "AddressID", "Preferences_Category" FROM public."Clients"'
+    ) AS c("ClientID" int, "Name" varchar, "Email" varchar, "PhoneNumber" varchar(20), "AddressID" int, "Preferences_Category" int)
+    LEFT JOIN DimClients dc ON dc.ClientSK = c."ClientID"
+    WHERE dc.ClientSK IS NULL OR (dc.IsCurrent AND (TRIM(c."Name") <> TRIM(dc.Name)
+        OR TRIM(c."PhoneNumber") <> TRIM(dc.PhoneNumber)
+        OR c."AddressID" <> dc.AddressSK
+        OR c."Preferences_Category" <> dc.PreferencesCategorySK))),
+update_customer AS (
+    UPDATE DimClients
+    SET 
+        IsCurrent = false,
+        EndDateID = (SELECT DatesKey FROM DimDates WHERE Date = CURRENT_DATE)
+    WHERE 
+        ClientSK IN (SELECT "ClientID" FROM updated_customers) AND IsCurrent = true
+)
+INSERT INTO DimClients (ClientSK ,Name, Email, PhoneNumber, AddressSK, PreferencesCategorySK, StartDateID, IsCurrent)
+    SELECT "ClientID", "Name", "Email", "PhoneNumber", "AddressID", "Preferences_Category", StartDateID, true
+    FROM updated_customers;
 
-INSERT INTO DimClients (ClientSK,Name, Email, PhoneNumber, AddressSK, PreferencesCategorySK, IsCurrent, StartDateID)
-SELECT DISTINCT 
-	c."ClientID",
-    c."Name", 
-    c."Email", 
-    c."PhoneNumber", 
-    C."AddressID", 
-    c."Preferences_Category", 
-    TRUE, 
-    (SELECT datesk FROM dimdates WHERE date = CURRENT_DATE)
-FROM dblink(
-    'dbname=oltp_database user=postgres password=123 host=localhost',
-    'SELECT "ClientID", "Name", "Email", "PhoneNumber", "AddressID", "Preferences_Category" FROM public."Clients"'
-) AS c("ClientID" int, "Name" varchar, "Email" varchar, "PhoneNumber" varchar, "AddressID" int, "Preferences_Category" int)
-ON CONFLICT (Email) DO UPDATE SET
-    Name = EXCLUDED.Name,
-    PhoneNumber = EXCLUDED.PhoneNumber,
-    AddressSK = EXCLUDED.AddressSK,
-    PreferencesCategorySK = EXCLUDED.PreferencesCategorySK,
-    IsCurrent = EXCLUDED.IsCurrent,
-    StartDateID = EXCLUDED.StartDateID
-WHERE 
-    DimClients.Name <> EXCLUDED.Name OR
-    DimClients.PhoneNumber <> EXCLUDED.PhoneNumber OR
-    DimClients.AddressSK <> EXCLUDED.AddressSK OR
-    DimClients.PreferencesCategorySK <> EXCLUDED.PreferencesCategorySK OR
-    DimClients.IsCurrent <> EXCLUDED.IsCurrent OR
-    DimClients.StartDateID <> EXCLUDED.StartDateID;
-
-INSERT INTO FactSales (DateSK, ProductSK, ClientSK, StatusSK, Quantity, TotalSalesAmount, PickUpSK)
+INSERT INTO FactSales (DatesKey, ProductSK, ClientSK, StatusSK, Quantity, TotalSalesAmount, PickUpSK)
 SELECT 
-    dd.DateSK,
+    dd.DatesKey,
     dp.ProductSK,
     dc.ClientSK,
     ds.StatusSK,
@@ -161,14 +165,14 @@ FROM dblink(
     'SELECT o."OrderDate", od."ProductID", od."Quantity", p."Price", o."ClientID", o."StatusId", o."PickUpOptions"
      FROM public."Orders" o
      JOIN public."OrderDetails" od ON o."OrderID" = od."OrderID"
-     JOIN public."Products" p ON p."ProductID" = od."ProductID"')
-AS od("OrderDate" date, "ProductID" int, "Quantity" int, "Price" decimal, "ClientID" int, "StatusId" int, "PickUpOptions" int)
-JOIN DimProducts dp ON dp.ProductSK = od."ProductID"
+     JOIN public."Products" p ON p."ProductID" = od."ProductID"'
+) AS od("OrderDate" date, "ProductID" int, "Quantity" int, "Price" decimal, "ClientID" int, "StatusId" int, "PickUpOptions" int)
+JOIN DimProducts dp ON dp.ProductSK = od."ProductID" and dp.IsCurrent
 JOIN DimClients dc ON dc.ClientSK = od."ClientID"
 JOIN DimStatus ds ON ds.StatusSK = od."StatusId"
 JOIN DimDates dd ON dd.Date = od."OrderDate"
 JOIN DimPickUp dpo ON dpo.PickUpSK = od."PickUpOptions"
-ON CONFLICT (ProductSK, ClientSK, DateSK) DO UPDATE SET 
+ON CONFLICT (ProductSK, ClientSK, DatesKey) DO UPDATE SET 
     Quantity = EXCLUDED.Quantity,
     TotalSalesAmount = EXCLUDED.TotalSalesAmount,
     StatusSK = EXCLUDED.StatusSK,
@@ -179,15 +183,15 @@ WHERE
     FactSales.StatusSK <> EXCLUDED.StatusSK OR
     FactSales.PickUpSK <> EXCLUDED.PickUpSK;
 
-INSERT INTO FactInventory (ProductSK, DateSK, StockQuantity)
+INSERT INTO FactInventory (ProductSK, DatesKey, StockQuantity)
 SELECT 
-    (SELECT ProductSK from DimProducts d where d.Name = "Name"),
-    (SELECT datesk FROM dimdates WHERE date = CURRENT_DATE),
+    (SELECT ProductSK FROM DimProducts dp WHERE dp.Name = p."Name" and dp.IsCurrent),
+    (SELECT DatesKey FROM DimDates dd WHERE dd.Date = CURRENT_DATE),
     p."StockQuantity"
 FROM dblink(
     'dbname=oltp_database user=postgres password=123 host=localhost',
-    'SELECT "Name", "StockQuantity" FROM "Products"'
+    'SELECT "Name", "StockQuantity" FROM public."Products"'
 ) AS p("Name" varchar, "StockQuantity" int)
-ON CONFLICT (DateSK, ProductSK) DO UPDATE
+ON CONFLICT (DatesKey, ProductSK) DO UPDATE
 SET StockQuantity = EXCLUDED.StockQuantity
 WHERE FactInventory.StockQuantity <> EXCLUDED.StockQuantity;
--- creation olap_database.sql
+++ creation olap_database.sql
@@ -14,45 +14,59 @@ DROP TABLE IF EXISTS
 CREATE EXTENSION IF NOT EXISTS dblink;
 
 CREATE TABLE DimCities (
-    CitySK SERIAL PRIMARY KEY,
-    CityName VARCHAR(255) NOT NULL);
+    CitiesKey SERIAL PRIMARY KEY,
+    CitySK INT NOT NULL,
+    CityName VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE DimAddresses (
-    AddressSK SERIAL PRIMARY KEY,
+    AddressesKey SERIAL PRIMARY KEY,
+    AddressSK INT NOT NULL,
     Street VARCHAR(255),
     CitySK INT NOT NULL,
     PostalCode VARCHAR(20),
     Country VARCHAR(100),
-    FOREIGN KEY (CitySK) REFERENCES DimCities(CitySK));
+    FOREIGN KEY (CitySK) REFERENCES DimCities(CitiesKey)
+);
 
 CREATE TABLE DimConditions (
-    ConditionSK SERIAL PRIMARY KEY,
-    Name VARCHAR(255) NOT NULL);
+    ConditionsKey SERIAL PRIMARY KEY,
+    ConditionSK INT NOT NULL,
+    Name VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE DimCategories (
-    CategorySK SERIAL PRIMARY KEY,
-    Name VARCHAR(255) NOT NULL);
+    CategoriesKey SERIAL PRIMARY KEY,
+    CategorySK INT NOT NULL,
+    Name VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE DimStatus (
-    StatusSK SERIAL PRIMARY KEY,
-    Name VARCHAR(255) NOT NULL);
+    StatusKey SERIAL PRIMARY KEY,
+    StatusSK INT NOT NULL,
+    Name VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE DimPickUp (
-    PickUpSK SERIAL PRIMARY KEY,
-    Name VARCHAR(255) NOT NULL);
+    PickUpKey SERIAL PRIMARY KEY,
+    PickUpSK INT NOT NULL,
+    Name VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE DimDates (
-    DateSK SERIAL PRIMARY KEY,
+    DatesKey SERIAL PRIMARY KEY,
     Date DATE NOT NULL,
     Year INT,
     Month INT,
     Day INT,
     Week INT,
-    Quarter INT);
+    Quarter INT
+);
 
 CREATE TABLE DimProducts (
-    ProductSK SERIAL PRIMARY KEY,
-    Name VARCHAR(255) unique NOT NULL,
+    ProductsKey SERIAL PRIMARY KEY,
+    ProductSK INT NOT NULL,
+    Name VARCHAR(255) NOT NULL,
     CategorySK INT NOT NULL,
     ConditionSK INT NOT NULL,
     Era VARCHAR(100) NOT NULL,
@@ -60,47 +74,52 @@ CREATE TABLE DimProducts (
     IsCurrent BOOLEAN NOT NULL DEFAULT TRUE,
     StartDateID INT NOT NULL,
     EndDateID INT,
-    FOREIGN KEY (CategorySK) REFERENCES DimCategories(CategorySK),
-    FOREIGN KEY (ConditionSK) REFERENCES DimConditions(ConditionSK),
-	FOREIGN KEY (StartDateID ) REFERENCES DimDates(DateSK),
-	FOREIGN KEY (EndDateID ) REFERENCES DimDates(DateSK));
+    FOREIGN KEY (CategorySK) REFERENCES DimCategories(CategoriesKey),
+    FOREIGN KEY (ConditionSK) REFERENCES DimConditions(ConditionsKey),
+    FOREIGN KEY (StartDateID) REFERENCES DimDates(DatesKey),
+    FOREIGN KEY (EndDateID) REFERENCES DimDates(DatesKey)
+);
 
 CREATE TABLE DimClients (
-    ClientSK SERIAL PRIMARY KEY,
+    ClientsKey SERIAL PRIMARY KEY,
+    ClientSK INT NOT NULL,
     Name VARCHAR(255) NOT NULL,
-    Email VARCHAR(255) unique,
+    Email VARCHAR(255),
     PhoneNumber VARCHAR(20),
     AddressSK INT NOT NULL,
     PreferencesCategorySK INT NOT NULL,
     IsCurrent BOOLEAN NOT NULL DEFAULT TRUE,
     StartDateID INT NOT NULL,
     EndDateID INT,
-    FOREIGN KEY (AddressSK) REFERENCES DimAddresses(AddressSK),
-    FOREIGN KEY (PreferencesCategorySK) REFERENCES DimCategories(CategorySK),
-	FOREIGN KEY (StartDateID ) REFERENCES DimDates(DateSK),
-	FOREIGN KEY (EndDateID ) REFERENCES DimDates(DateSK));
+    FOREIGN KEY (AddressSK) REFERENCES DimAddresses(AddressesKey),
+    FOREIGN KEY (PreferencesCategorySK) REFERENCES DimCategories(CategoriesKey),
+    FOREIGN KEY (StartDateID) REFERENCES DimDates(DatesKey),
+    FOREIGN KEY (EndDateID) REFERENCES DimDates(DatesKey)
+);
 
 CREATE TABLE FactSales (
     SalesID SERIAL PRIMARY KEY,
-    DateSK INT NOT NULL,
+    DatesKey INT NOT NULL,
     ProductSK INT NOT NULL,
     ClientSK INT NOT NULL,
     StatusSK INT NOT NULL,
-	PickUpSK INT NOT NULL,
+    PickUpSK INT NOT NULL,
     Quantity INT NOT NULL,
     TotalSalesAmount DECIMAL(12, 2),
-    FOREIGN KEY (DateSK) REFERENCES DimDates(DateSK),
-    FOREIGN KEY (ProductSK) REFERENCES DimProducts(ProductSK),
-    FOREIGN KEY (ClientSK) REFERENCES DimClients(ClientSK),
-    FOREIGN KEY (StatusSK) REFERENCES DimStatus(StatusSK),
-	FOREIGN KEY (PickUpSK) REFERENCES DimPickUp(PickUpSK),
-    UNIQUE (ClientSK, ProductSK, DateSK));
+    FOREIGN KEY (DatesKey) REFERENCES DimDates(DatesKey),
+    FOREIGN KEY (ProductSK) REFERENCES DimProducts(ProductsKey),
+    FOREIGN KEY (ClientSK) REFERENCES DimClients(ClientsKey),
+    FOREIGN KEY (StatusSK) REFERENCES DimStatus(StatusKey),
+    FOREIGN KEY (PickUpSK) REFERENCES DimPickUp(PickUpKey),
+    UNIQUE (ClientSK, ProductSK, DatesKey)
+);
 
 CREATE TABLE FactInventory (
     InventoryID SERIAL PRIMARY KEY,
     ProductSK INT NOT NULL,
-    DateSK INT NOT NULL,
+    DatesKey INT NOT NULL,
     StockQuantity INT NOT NULL,
-    FOREIGN KEY (ProductSK) REFERENCES DimProducts(ProductSK),
-    FOREIGN KEY (DateSK) REFERENCES DimDates(DateSK),
-    UNIQUE (DateSK, ProductSK));
+    FOREIGN KEY (ProductSK) REFERENCES DimProducts(ProductsKey),
+    FOREIGN KEY (DatesKey) REFERENCES DimDates(DatesKey),
+    UNIQUE (DatesKey, ProductSK)
+);
